76768bd845f133178f0036b59a9d8b6c6040a781
Mohamed Tharwat <mthawrat@gmail.com>
2026-08-26 12:07:44 +0300

    fix: add production and local origins to CORS whitelist

--- src/index.js
+++ src/index.js
@@ -36,6 +36,14 @@ import quickTipRoutes from './modules/quick-tips/routes/quickTip.route.js';
 // cron jobs
 import { initScraper } from './modules/scraper/controllers/scraper.controller.js';
 
+const allowedOrigins = [
+    'https://callavee.com',
+    'https://www.callavee.com',
+    'https://admin.callavee.com',
+    'http://localhost:3000',
+    'http://localhost:5173'
+];
+
 
 const app = express();
 
@@ -45,10 +53,15 @@ app.use(cookieParser());
 app.set('trust proxy', 1); 
 app.use(helmet());
 app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL,
-        process.env.DASHBOARD_URL
-    ],
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
